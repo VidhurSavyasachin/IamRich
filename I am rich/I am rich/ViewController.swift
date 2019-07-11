2b5ae9104c5dca76214487ea9408149d34ce40a9
Vidhur Savyasachin <vidhursachin@gmail.com>
2019-07-11 03:22:29 -0400

    Add files via upload

---
+++ I am rich/I am rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Vidhur Savyasachin on 6/6/19.
+//  Copyright © 2019 Vidhur Savyasachin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
